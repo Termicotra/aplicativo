bc702cbb63014be59b5f62ffc1b68545cc5ab377
Fede Traversi <federicotraversir99@hotmail.com>
2026-09-16 15:17:27 -0300

    Fix migration 0002: handle missing tables gracefully with SQL IF EXISTS

--- evaluaciones/migrations/0002_rename_tables.py
+++ evaluaciones/migrations/0002_rename_tables.py
@@ -8,16 +8,20 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterModelTable(
-            name='ejercicio',
-            table='ejercicio_evaluacion',
-        ),
-        migrations.AlterModelTable(
-            name='opcionejercicio',
-            table='opcion_evaluacion',
-        ),
-        migrations.AlterModelTable(
-            name='respuestaejercicio',
-            table='respuesta_evaluacion',
+        migrations.RunSQL(
+            sql="""
+            DO $$ BEGIN
+                IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'ejercicio_capacitacion') THEN
+                    ALTER TABLE IF EXISTS ejercicio_capacitacion RENAME TO ejercicio_evaluacion;
+                END IF;
+                IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'opcion_ejercicio') THEN
+                    ALTER TABLE IF EXISTS opcion_ejercicio RENAME TO opcion_evaluacion;
+                END IF;
+                IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'respuesta_capacitacion') THEN
+                    ALTER TABLE IF EXISTS respuesta_capacitacion RENAME TO respuesta_evaluacion;
+                END IF;
+            END $$;
+            """,
+            reverse_sql="",
         ),
     ]
